f6f4be61f26d1c182c5f6d1f6f391aebac65f7d5
Ricky Thomas <rickythomas.rt@gmail.com>
2015-01-23 15:00:13 -0500

    adds survey.rb file and title_form file

---
+++ app/controllers/survey.rb
@@ -0,0 +1,13 @@
+get "/:id/survey" do |id|
+  @current_user = User.find(id)
+  erb :title_form
+end
+
+post "/:id/survey/new" do |id|# User creates a new survey.
+  @new_survey = Survey.create(title: params['new_survey']['title'], user_id: id)
+  redirect("#{id}/survey/#{@new_survey.id}/edit")
+end
+
+get "/:user_id/survey/:survey_id/edit" do |user_id, survey_id|
+
+end
