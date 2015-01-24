2744b4a3d440452241a225d18984f51f0ce3288a
Kevin Perumal & Dionne Stanfield <pair+kevinperumal.sputterputtredux@devbootcamp.com>
2015-01-24 15:10:30 -0500

    correct instance variable syntax.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -11,8 +11,8 @@ end
 post '/signup' do
   @user = User.create(params[:user])
 
-  if user.save
-    session[:user_id] = user.id
+  if @user.save
+    session[:user_id] = @user.id
     redirect "/"
 
   else
